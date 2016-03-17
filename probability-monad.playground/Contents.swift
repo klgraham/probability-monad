039508d1341426381b6f59f43858328c400c6bc7
Kenneth Graham <klgraham@gmail.com>
2016-03-17 08:54:20 -0500

    Added beginnings of a probability monda in swift, using protocol-oriented programming

---
+++ probability-monad.playground/Contents.swift
@@ -0,0 +1,75 @@
+
+import Foundation
+
+protocol RandomVariable {
+    associatedtype NumberType
+    func get() -> NumberType
+    func sample(n: Int) -> [NumberType]
+}
+
+protocol Parameterizable {
+    associatedtype ParameterType
+    var p: ParameterType { get }
+}
+
+//protocol Transformable {
+//    associatedtype A
+//    associatedtype B
+//    func map<B>(f: A -> B) -> Transformable
+//}
+
+struct UniformDoubleDist: RandomVariable {
+    // Returns a uniform double on [0,1]
+    func get() -> Double {
+        return drand48()
+    }
+    
+    func sample(n: Int) -> [Double] {
+        return (1...n).map { x in get() }
+    }
+}
+
+let uniform = UniformDoubleDist()
+uniform.get()
+uniform.sample(2)
+
+struct BooleanDist: RandomVariable, Parameterizable {
+    var p: Double
+    let uniform = UniformDoubleDist()
+    
+    func get() -> Bool {
+        return uniform.get() < p
+    }
+    
+    func sample(n: Int) -> [Bool] {
+        return uniform.sample(n).map { x in x < p }
+    }
+}
+
+let tf = BooleanDist(p: 0.8)
+tf.sample(10)
+
+struct BernoulliDist: RandomVariable, Parameterizable {
+    var p: Double
+    let uniform = UniformDoubleDist()
+    
+    func get() -> Int {
+        return oneOrZero(uniform.get() < p)
+    }
+    
+    func sample(n: Int) -> [Int] {
+        return uniform.sample(n).map { x in x < p }.map { x in oneOrZero(x) }
+    }
+        
+    private func oneOrZero(x: Bool) -> Int {
+        if (x) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+}
+
+let bernoulli = BernoulliDist(p: 0.5)
+bernoulli.sample(10)
+
