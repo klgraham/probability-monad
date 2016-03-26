a75d5ea6fab99b0a28133d5e800587df6b8e287b
Kenneth Graham <klgraham@gmail.com>
2016-03-25 21:36:04 -0500

    Added mean, variance, standard deviation, way to restrict samples from a distribution with a predicate. Cleaned up closures.

--- probability-monad.playground/Contents.swift
+++ probability-monad.playground/Contents.swift
@@ -26,11 +26,39 @@ struct Distribution<A> {
     }
     
     let N = 10000
+    // probability of the predicate being true
     func prob(predicate: A -> Bool) -> Double {
         return Double(sample(N).filter(predicate).count) / Double(N)
     }
     
+    // Samples from the new distribution so that the result matches the predicate
+    func given(predicate: A -> Bool) -> Distribution<A> {
+        var d = self
+        let a = d.get()!
+        d.get = { predicate(a) ? a : self.get() }
+        return d
+    }
     
+    func mean() -> Double {
+        return sample(N).reduce(0, combine: { $0 + Double(String($1))! }) / Double(N)
+    }
+    
+    func variance() -> Double {
+        var sum: Double = 0
+        var sqrSum: Double = 0
+        
+        for x in sample(N) {
+            let xx = Double(String(x))!
+            sum += xx
+            sqrSum += xx * xx
+        }
+        
+        return (sqrSum - sum * sum / Double(N)) / Double(N-1)
+    }
+    
+    func stdDev() -> Double {
+        return sqrt(self.variance())
+    }
 }
 
 // random number generation functions
@@ -48,7 +76,7 @@ func nextInt(min min: Int, max: Int) -> ((Void) -> Int) {
 // transformation functions
 
 func lessThan(p: Double) -> (Double -> Bool) {
-    return { x in return x < p }
+    return { $0 < p }
 }
 
 let u = Distribution<Double>(get: nextDouble)
@@ -56,11 +84,14 @@ u.get()
 u.get()
 u.get()
 
+//u.mean()
+
+
 let tf = u.map(lessThan(0.75))
 tf.get()
 tf.sample(5)
 
-let bernoulli1 = tf.map({(b: Bool) in return b ? 1 : 0})
+let bernoulli1 = tf.map({$0 ? 1 : 0})
 bernoulli1.sample(10)
 
 let die6 = Distribution<Int>(get: nextInt(min: 1, max: 6))
@@ -73,7 +104,7 @@ let pair = die6.flatMap({
 pair.sample(7)
 
 // probability computations
-print(tf.prob({ (x : Bool) in return x }))
+//print(tf.prob({ $0 }))
 
 
 // discrete distributions
